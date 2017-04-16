36e8d85bf78f2627db4f3df54045562cdf95f6cd
A. K. M. NAZMUL ISLAM <nazmulislamredoy@gmail.com>
2017-04-16 12:20:23 +0600

    latest update

--- resources/js/custom.js
+++ resources/js/custom.js
@@ -1,3 +1,21 @@
+
+$(function() {
+    $('#login-form-link').click(function(e) {
+		$("#login-form").delay(100).fadeIn(100);
+ 		$("#register-form").fadeOut(100);
+		$('#register-form-link').removeClass('active');
+		$(this).addClass('active');
+		e.preventDefault();
+	});
+	$('#register-form-link').click(function(e) {
+		$("#register-form").delay(100).fadeIn(100);
+ 		$("#login-form").fadeOut(100);
+		$('#login-form-link').removeClass('active');
+		$(this).addClass('active');
+		e.preventDefault();
+	});
+
+});
 $(function() {
     $("#header-section").load("template/section/header.html");
     $("#footer-section").load("template/section/footer.html");
